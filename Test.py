9ba97f0f7af031531e9e9d3314a9461d7bc04790
fcamacho <f.camacho10@uniandes.edu.co>
2022-03-20 09:00:07 -0500

    first commit

---
+++ Test.py
@@ -0,0 +1,23 @@
+print ("Hol2")
+
+a=2
+b=5
+
+print(a+b)
+
+import arcpy
+arcpy.FeatureClassToFeatureClass_conversion()
+
+for test in pruba:
+    if a = 5:
+        print()
+        []
+
+( ) ()
+    ()
+    [}
+    }ñ
+    {
+
+        {}
+    }]
