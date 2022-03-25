95c52986c3198cebe73a561b70772bb1b1931733
fcamacho <f.camacho10@uniandes.edu.co>
2022-03-25 09:53:50 -0500

    finalsql

---
+++ Cursos_SQL/Curso_de_SQL_y_MYSQL/18_tipos_de_join.sql
@@ -0,0 +1,4 @@
+"Tenemos Tabla a y tabla b"
+
+1. "Inner join"
+    
--- Cursos_SQL/Curso_de_SQL_y_MYSQL/22_Super_Queries.sql
+++ Cursos_SQL/Curso_de_SQL_y_MYSQL/22_Super_Queries.sql
@@ -4,4 +4,4 @@ sum(if(year <1950 and year <2000, 1, 0)) as 'MAYOR 1950 pero menor 2000'
 from books as b
 join authors as a ON a.author_id = b.author_id
 where a.nationality is not null
-group  by book_id
+group  by a.nationality; 
